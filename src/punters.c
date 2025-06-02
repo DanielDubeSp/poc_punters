1e2fc54c46752981020a3f575791ad1ba0947efa
Daniel Dubé Spielvogel <52450079+DanielDubeSp@users.noreply.github.com>
2025-06-02 22:30:16 +0000

    Fourth

--- src/punters.c
+++ src/punters.c
@@ -11,12 +11,12 @@ int main()
 	int* ptr = &myAge;  // A pointer variable, with the name ptr, that stores the address of myAge
 
 	// Output the value of myAge (43)
-	printf("%d\n", myAge);
+	printf("%d\n   ", myAge);
 	// Output the memory address of myAge (0x7ffe5367e044)
-	printf("%p\n", &myAge);
+	printf("%p\n   ", &myAge);
 	// Output the memory address of myAge with the pointer (0x7ffe5367e044)
-	printf("%p\n", ptr);	
+	printf("%p\n   ", ptr);	
 	// Dereference: Output the value of myAge with the pointer (43)
-	printf("%d\n", *ptr);	
+	printf("%d\n   ", *ptr);	
 	return 0;
 }
