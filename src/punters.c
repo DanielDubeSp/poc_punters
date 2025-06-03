ccd211d09fa042c3205d25bb3ea084cc7a7d7ff2
Daniel Dubé Spielvogel <52450079+DanielDubeSp@users.noreply.github.com>
2025-06-03 13:55:32 +0000

    Move to docker-compose

--- src/punters.c
+++ src/punters.c
@@ -7,16 +7,22 @@ int main()
 {
 	printf("Hello World\n");
 
-	int myAge = 43;     // An int variable
-	int* ptr = &myAge;  // A pointer variable, with the name ptr, that stores the address of myAge
+	char myChar = 3325522;  // A variable
+
+	double myAge = 43.10000004;     // A variable
+	double* ptr = &myAge;  // A pointer variable, with the name ptr, that stores the address of myAge
 
 	// Output the value of myAge (43)
-	printf("%d\n", myAge);
+	printf("%f years old\n", myAge);
+	
+	// Size of myAge (8)
+	printf("%lu bytes\n", sizeof(myAge));
+
 	// Output the memory address of myAge (0x7ffe5367e044)
 	printf("%p\n", &myAge);
 	// Output the memory address of myAge with the pointer (0x7ffe5367e044)
 	printf("%p\n", ptr);	
 	// Dereference: Output the value of myAge with the pointer (43)
-	printf("%d\n", *ptr);	
+	printf("%f years olda\n", *ptr);	
 	return 0;
 }
